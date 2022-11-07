1e52936cb9d364d4cdcbdaee89994e862f21e47f
HansanaBackup <hansana1234dasanayake@gmail.com>
2022-11-07 19:24:34 +0530

    body-parser added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const port = 8000;
 
-app.listen(8000, function () {
-  console.log("Server Started on port 8000.");
+app.listen(port, () => {
+  console.log(`Server Started on port ${port} .`);
 });
